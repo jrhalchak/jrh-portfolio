7aaf49b619de8b34d5fefcd2e0f5d616ebfc5bdf
Jonathan Halchak <jhalchak@arhaus.com>
2016-04-28 16:37:52 -0400

    Entries app placeholder, admin route additions for entry

--- routes/admin.js
+++ routes/admin.js
@@ -13,10 +13,12 @@ router.get('/', helpers.isAuthenticated, function(req, res, next) {
   providers.page.getPageList((err, pageResults)=> {
     if(err) console.log(err);
     //get entries here
-    proivders.entry.getEntryList((err, entryResults))
-    viewModel.pages = pageResults;
-    viewModel.entries = entryResults;
-    res.render('admin/index', viewModel);
+    proivders.entry.getEntryList((err, entryResults)=> {
+        if(err) console.log(err);
+        viewModel.pages = pageResults;
+        viewModel.entries = entryResults;
+        res.render('admin/index', viewModel); 
+    });
   });
 });
 
@@ -44,6 +46,35 @@ router.post('/page/edit/:id', helpers.isAuthenticated, (req,res,next)=>{
       result.body =  req.body.page_body || result.body;
       result.pageType =  req.body.page_type || result.pageType;
       result.pageBgCSS =  req.body.page_bg_css || result.pageBgCSS;
+      result.save();
+      res.redirect('/admin');
+    }
+  });
+});
+
+router.get('/entry/edit/:id', helpers.isAuthenticated, (req,res,next)=>{
+  providers.entry.getEntryById(req.params.id, (err, result)=> {
+    if(err) console.log(err);
+    if(!result) {
+      res.status(404).send('Not found!');
+      return;
+    } else {
+      viewModel.pages[0] = result;
+      res.render('admin/edit_page', viewModel);
+    }
+  });
+});
+
+router.post('/entry/edit/:id', helpers.isAuthenticated, (req,res,next)=>{
+  providers.entry.getEntryById(req.params.id, (err, result)=> {
+    if(err) console.log(err);
+    if(!result) {
+      res.status(404).send('Not found!');
+      return;
+    } else {
+      result.title = req.body.entry_title || result.title;
+      result.body =  req.body.entry_body || result.body;
+      result.entryBgCSS =  req.body.entry_bg_css || result.entryBgCSS;
       result.save();
       res.redirect('/admin');
     }
